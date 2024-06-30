69172e115dd8c2e422396ee38b718914c7bdaee0
Tyler Hebenstreit <tyler.hebenstreit@breaktech.com>
2024-06-30 12:44:18 -0500

    Updater update for HB sync

--- src/pages/Updater.js
+++ src/pages/Updater.js
@@ -14,7 +14,7 @@ const Updater = () => {
   } = useContext(ConfigContext);
   const { showAlert } = useContext(AlertContext);
   const [log, setLog] = useState([]);
-  const [updates, setUpdates] = useState([]);
+  const [updates, setUpdates] = useState({ goconf: [], hb: [] });
   const [updating, setUpdating] = useState(false);
   const [gettingUpdates, setGettingUpdates] = useState(false);
 
@@ -23,7 +23,7 @@ const Updater = () => {
   const govee = getGovee();
 
   const handleUpdate = async () => {
-    if (updates.length < 1) {
+    if (updates.goconf.length < 1 && updates.hb.length < 1) {
       showAlert('error', 'No updates available');
       return;
     }
@@ -31,9 +31,8 @@ const Updater = () => {
     const toLog = [];
     const { lightDevices } = hb.pluginConfig;
     const sceneSlots = await goconf.reload();
-    updates.forEach((update) => {
-      let success = false;
-      let index = sceneSlots.findIndex(
+    updates.goconf.forEach((update) => {
+      const index = sceneSlots.findIndex(
         (ss) => ss.scene === update.sceneName && ss.room === update.room && ss.slot === update.slot,
       );
       if (index > -1) {
@@ -42,10 +41,8 @@ const Updater = () => {
           if (update.code) {
             sceneSlots[index].devices[ssdIndex].code = update.code;
             sceneSlots[index].devices[ssdIndex].diyName = update.diyName;
-            success = true;
           } else {
             sceneSlots[index].devices.splice(ssdIndex, 1);
-            success = true;
           }
         } else {
           sceneSlots[index].devices.push({
@@ -55,18 +52,22 @@ const Updater = () => {
           });
         }
       }
+    });
 
-      index = lightDevices.findIndex((light) => light.label === update.device);
+    updates.hb.forEach(({
+      scene, slot, device, sceneCode,
+    }) => {
+      const index = lightDevices.findIndex((light) => light.label === device);
       if (index >= 0) {
-        if (update.code) {
-          lightDevices[index][update.slot] = {
-            sceneCode: update.code,
+        if (sceneCode) {
+          lightDevices[index][slot] = {
+            sceneCode,
             showAs: 'switch',
           };
-          toLog.push(`${!success ? '[HB ONLY] ' : ''} Updated ${update.device} for scene: ${update.sceneName}`);
+          toLog.push(`[HB] Updated ${device} for scene: ${scene}`);
         } else {
-          delete lightDevices[index][update.slot];
-          toLog.push(`${!success ? '[HB ONLY] ' : ''} Deleted ${update.device} from: ${update.sceneName}`);
+          delete lightDevices[index][slot];
+          toLog.push(`[HB] Deleted ${device} from: ${scene}`);
         }
       }
     });
@@ -92,22 +93,22 @@ const Updater = () => {
     const toLog = [];
     const { scenes } = govee;
     const sceneSlots = await goconf.reload();
+    const { lightDevices } = hb.pluginConfig;
+    const hbDevices = [...lightDevices];
+    const hbUpdates = [];
     sceneSlots.forEach((sceneSlot) => {
       const sceneName = (sceneSlot.room === 'office' ? 'Office ' : '') + sceneSlot.scene;
       if (sceneSlot.scene) {
-        const sceneDevices = sceneSlot?.devices || [];
+        const goconfSceneDevices = sceneSlot?.devices || [];
         const ttrScene = scenes[sceneName];
         if (ttrScene && ttrScene?.devices) {
-          if (sceneName === 'Dark Rainbow') {
-            console.log('Dark Rainbow sceneSlot', sceneSlot, 'ttrScene', ttrScene);
-          }
           // we have a govee TTR scene and a list of goconf scene devices
           // devices in the goconf scene not found in the govee scene need to be REMOVED from goconf scene
           // devices in the govee scene not found in the goconf scene need to be ADDED to the goconf scene
           const ttrSceneDevices = ttrScene?.devices || {};
           Object.keys(ttrSceneDevices).forEach((device) => {
             // for each GOVEE device, check for changed codes, or new devices
-            const sdIndex = sceneDevices.findIndex((sd) => sd.device === device);
+            const sdIndex = goconfSceneDevices.findIndex((sd) => sd.device === device);
             if (sdIndex === -1) {
               // add this device to GOCONF
               toUpdate.push({
@@ -119,7 +120,7 @@ const Updater = () => {
                 slot: sceneSlot.slot,
               });
               toLog.push(`[${sceneSlot.scene}][${sceneSlot.room}] Device added: ${device}`);
-            } else if (sceneDevices[sdIndex].code !== ttrSceneDevices[device].code) {
+            } else if (goconfSceneDevices[sdIndex].code !== ttrSceneDevices[device].code) {
               // update the code for this device in GOCONF
               toUpdate.push({
                 sceneName: sceneSlot.scene,
@@ -132,7 +133,7 @@ const Updater = () => {
               toLog.push(`[${sceneSlot.scene}][${sceneSlot.room}] Scene code changed for: ${device}`);
             }
           });
-          sceneDevices.forEach(({ device, code }) => {
+          goconfSceneDevices.forEach(({ device, code }) => {
             // for each GOCONF scene device, check for deleted devices
             if (!ttrSceneDevices[device]?.code) {
               toUpdate.push({
@@ -144,13 +145,49 @@ const Updater = () => {
               });
               toLog.push(`[${sceneSlot.scene}][${sceneSlot.room}] Device removed: ${device}`);
             }
+
+            // check for differences in the HB light devices
+            const lightDevice = hbDevices.find((ld) => ld.label === device);
+            if (lightDevice) {
+              const lightDeviceSlot = lightDevice[sceneSlot.slot];
+              if (lightDeviceSlot) {
+                if (code !== lightDeviceSlot.sceneCode) {
+                  hbUpdates.push({
+                    sceneCode: ttrSceneDevices[device]?.code ? code : null,
+                    slot: sceneSlot.slot,
+                    device,
+                    room: sceneSlot.room,
+                    scene: sceneSlot.scene,
+                  });
+                }
+              } else {
+                toLog.push(`[${sceneSlot.scene}][${device}] Homebridge missing device slot: ${sceneSlot.slot}`);
+                hbUpdates.push({
+                  sceneCode: ttrSceneDevices[device]?.code ? code : null,
+                  slot: sceneSlot.slot,
+                  device,
+                  room: sceneSlot.room,
+                  scene: sceneSlot.scene,
+                });
+              }
+            } else {
+              toLog.push(`[${sceneSlot.scene}][${device}] Homebridge missing device`);
+            }
           });
         } else {
-          updates.push(`[${sceneSlot.scene}][${sceneSlot.room}] ttrScene not found for device`);
+          toLog.push(`[${sceneSlot.scene}][${sceneSlot.room}] ttrScene not found for device`);
         }
       }
     });
-    setUpdates(toUpdate);
+    toUpdate.forEach(({ code, device, slot }) => {
+      const index = hbUpdates.findIndex((hbu) => hbu.device === device && hbu.slot === slot);
+      if (index > -1) {
+        hbUpdates[index].sceneCode = code;
+      }
+    });
+    console.log('toUpdate', toUpdate);
+    console.log('hbUpdates', hbUpdates);
+    setUpdates({ goconf: toUpdate, hb: hbUpdates });
     if (!toUpdate.length) {
       toLog.push('No updates found');
     }
