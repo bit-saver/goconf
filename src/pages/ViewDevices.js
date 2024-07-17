caac0afd47324337d243d5305365c5064909b34c
Tyler Hebenstreit <tyler.hebenstreit@breaktech.com>
2024-07-16 19:55:26 -0500

    Device page update with dropdown

--- src/pages/ViewDevices.js
+++ src/pages/ViewDevices.js
@@ -1,13 +1,15 @@
 import {
-  Card, CardHeader, Grid, List, ListItem, ListItemText,
+  Card, CardHeader, FormControl, Grid, InputLabel, List, ListItem, ListItemText, MenuItem, Select,
 } from '@mui/material';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ConfigContext from '../util/contexts/ConfigContext';
 import PageTitle from '../components/PageTitle';
 
 const ViewDevices = () => {
   const { getHb } = useContext(ConfigContext);
   const hb = getHb();
+
+  const [selectedDevice, setSelectedDevice] = useState(null);
 
   return (
     <Grid container item spacing={4} xs={12}>
@@ -16,21 +18,42 @@ const ViewDevices = () => {
           title="View Devices"
         />
       </Grid>
-      {Object.keys(hb.devices).map((deviceName) => {
-        const device = hb.devices[deviceName];
-        return (
-          <Grid item xs={12} sm={4} md={3} lg={2} key={`slots-${deviceName}`}>
-            <CardHeader title={deviceName} />
+      <Grid container item xs={12}>
+        <Grid item xs={12}>
+          <Grid item xs={12} sm={8} md={8} lg={6}>
+            <FormControl fullWidth>
+              <InputLabel id="label-device">Device</InputLabel>
+              <Select
+                labelId="label-device"
+                id="select-device"
+                value={selectedDevice}
+                label="Device"
+                fullWidth
+                placeholder="Select Device"
+                onChange={(e) => setSelectedDevice(e.target.value)}
+              >
+                {Object.keys(hb.devices).map((device) => (
+                  <MenuItem value={device} key={device}>
+                    { device }
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+        {selectedDevice && (
+          <Grid item xs={12} sm={8} md={8} lg={6} key={`slots-${selectedDevice}`}>
+            <CardHeader title={selectedDevice} />
             <Card variant="outlined">
               <List>
-                {Object.keys(device.slots).map((slotName) => {
-                  const slot = device.slots[slotName];
+                {Object.keys(hb.devices[selectedDevice].slots).map((slotName) => {
+                  const slot = hb.devices[selectedDevice].slots[slotName];
                   let label = `[${slotName}]`;
                   if (slot) {
                     label = `[${slotName}] ${slot.sceneName}`;
                   }
                   return (
-                    <ListItem key={`${device}${slotName}`}>
+                    <ListItem key={`${selectedDevice}${slotName}`}>
                       <ListItemText primary={label} />
                     </ListItem>
                   );
@@ -38,8 +61,8 @@ const ViewDevices = () => {
               </List>
             </Card>
           </Grid>
-        );
-      })}
+        )}
+      </Grid>
     </Grid>
   );
 };
