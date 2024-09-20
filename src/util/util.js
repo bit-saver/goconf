0a861f34e2af4d3cca408ecb99d0e785aa435b61
bit-saver <tydahe@gmail.com>
2024-09-19 21:57:50 -0500

    Added cross strip

--- src/util/util.js
+++ src/util/util.js
@@ -81,8 +81,7 @@ export const defaultSlots = [
 
 export const deviceRooms = {
   'Beam Strip': 'living_room',
-  'Beam Left Strip': 'living_room',
-  'Beam Right Strip': 'living_room',
+  'Cross Strip': 'living_room',
   'TV Strip': 'living_room',
   'Kitchen Strip': 'living_room',
   'Window Strip': 'living_room',
